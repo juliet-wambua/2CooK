837ada52300c9a5079301dd60f7a6543f3c47ee3
juliet-wambua <julienea289@gmail.com>
2019-10-30 09:36:32 +0300

    week3

--- app/src/androidTest/java/com/nea/2CooK/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nea/2CooK/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.techspaceke.cookit", appContext.getPackageName());
+        assertEquals("com.nea.2CooK", appContext.getPackageName());
     }
 }
